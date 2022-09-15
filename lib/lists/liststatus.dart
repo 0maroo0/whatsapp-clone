78505ed7a9e6ede7c9a0f552650667db068f2a2d
0maroo0 <hm128652@gmail.com>
2022-09-15 13:01:16 +0200

    first commit

--- lib/lists/liststatus.dart
+++ lib/lists/liststatus.dart
@@ -6,45 +6,60 @@ List<modelStatus> liststatus = [
       name: "ahmed ",
       time: '2 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296101279_1532308990516316_550304841361999915_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF8JRAYTCxArkAPtZFEuuVrE-SvCNO-lBYT5K8I076UFrKzyFrQm5xgNNioyM8xUZVkPn68n0t_gKr253PJVhgs&_nc_ohc=bDrqghR8PukAX8F9vJ5&_nc_ht=scontent.fcai19-4.fna&oh=00_AT9sMWNzYPghj5ih84SPDLfdhvM5aVKseoeZVdF9HD_Rkg&oe=63197455'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296101279_1532308990516316_550304841361999915_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF8JRAYTCxArkAPtZFEuuVrE-SvCNO-lBYT5K8I076UFrKzyFrQm5xgNNioyM8xUZVkPn68n0t_gKr253PJVhgs&_nc_ohc=WaEL6wVbFoEAX-ULUAG&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-m3tkknbcvYY52FHWRQL1uyMijTZW_6eZyepljsVgTtg&oe=632551D5'),
   modelStatus(
       name: "Khedr",
       time: '7 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296285903_821179079248785_3145060403143889362_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGW3tUDmYT-6qZRbhvAk4TmmhPiuhay1NeaE-K6FrLU19hOC42raNdIRoeg4Ho-_feeGMZj8IH98uFj3yt5aC5K&_nc_ohc=epGtGKE1i_8AX_f_d64&_nc_ht=scontent.fcai19-4.fna&oh=00_AT9-O7d3HfMAdIgyXwMm7vcz7jvaxZMTD69Qgd-aECbySg&oe=6317C937'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/273571102_704287967604564_5995356503563951538_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFRZFI89AoUyQOunlL1GkoCLiExBLPAXQ0uITEEs8BdDcRxcSYNIFLCpRacQ9BXgO7lAgrRa0q4jeFJ80iyndu5&_nc_ohc=ZVcuvbyHpjYAX8w0juj&_nc_ht=scontent.fcai19-4.fna&oh=00_AT_7TuDz-2OlIEDlowsnRbGmB8PbV2_IFY3olybD6ezyZQ&oe=63254855'),
   modelStatus(
       name: "yousry",
       time: '8 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/297986257_1754739734889082_476247543936616366_n.jpg?stp=cp1_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeESyQV4szGpzjWaZWIsw6Fa2Qs83x9Hp_LZCzzfH0en8np2iMfUfPZmK6qYKFul-fzO7lZ_p2U8bo8s3VmzFsSx&_nc_ohc=hGmfXujmxugAX8gDjg1&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-59jfl2G36pSV4RFJ0PECd7LCdBpAoTGQw9-oKX-awcw&oe=6318B2CF'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/295904196_1746690802360642_7704700259405042136_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEvGOKU32Y7xOQMIHIIlVk92nCCYRmHePXacIJhGYd49SYf4UQ68T-vVoyyxUHSenoa9ExndL0FL2-DCq_f6J8t&_nc_ohc=1LR0ZwO7DUcAX-qNQ-U&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-WoD3pdbPK0zzF7FYOBTCX3y817m3FpPvZ6J4eUEhsQQ&oe=632559B6'),
   modelStatus(
       name: "ahmed ",
       time: '2 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296101279_1532308990516316_550304841361999915_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF8JRAYTCxArkAPtZFEuuVrE-SvCNO-lBYT5K8I076UFrKzyFrQm5xgNNioyM8xUZVkPn68n0t_gKr253PJVhgs&_nc_ohc=bDrqghR8PukAX8F9vJ5&_nc_ht=scontent.fcai19-4.fna&oh=00_AT9sMWNzYPghj5ih84SPDLfdhvM5aVKseoeZVdF9HD_Rkg&oe=63197455'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296101279_1532308990516316_550304841361999915_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF8JRAYTCxArkAPtZFEuuVrE-SvCNO-lBYT5K8I076UFrKzyFrQm5xgNNioyM8xUZVkPn68n0t_gKr253PJVhgs&_nc_ohc=WaEL6wVbFoEAX-ULUAG&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-m3tkknbcvYY52FHWRQL1uyMijTZW_6eZyepljsVgTtg&oe=632551D5'),
   modelStatus(
       name: "Khedr",
       time: '7 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296285903_821179079248785_3145060403143889362_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGW3tUDmYT-6qZRbhvAk4TmmhPiuhay1NeaE-K6FrLU19hOC42raNdIRoeg4Ho-_feeGMZj8IH98uFj3yt5aC5K&_nc_ohc=epGtGKE1i_8AX_f_d64&_nc_ht=scontent.fcai19-4.fna&oh=00_AT9-O7d3HfMAdIgyXwMm7vcz7jvaxZMTD69Qgd-aECbySg&oe=6317C937'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/273571102_704287967604564_5995356503563951538_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFRZFI89AoUyQOunlL1GkoCLiExBLPAXQ0uITEEs8BdDcRxcSYNIFLCpRacQ9BXgO7lAgrRa0q4jeFJ80iyndu5&_nc_ohc=ZVcuvbyHpjYAX8w0juj&_nc_ht=scontent.fcai19-4.fna&oh=00_AT_7TuDz-2OlIEDlowsnRbGmB8PbV2_IFY3olybD6ezyZQ&oe=63254855'),
   modelStatus(
       name: "yousry",
       time: '8 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/297986257_1754739734889082_476247543936616366_n.jpg?stp=cp1_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeESyQV4szGpzjWaZWIsw6Fa2Qs83x9Hp_LZCzzfH0en8np2iMfUfPZmK6qYKFul-fzO7lZ_p2U8bo8s3VmzFsSx&_nc_ohc=hGmfXujmxugAX8gDjg1&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-59jfl2G36pSV4RFJ0PECd7LCdBpAoTGQw9-oKX-awcw&oe=6318B2CF'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/295904196_1746690802360642_7704700259405042136_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEvGOKU32Y7xOQMIHIIlVk92nCCYRmHePXacIJhGYd49SYf4UQ68T-vVoyyxUHSenoa9ExndL0FL2-DCq_f6J8t&_nc_ohc=1LR0ZwO7DUcAX-qNQ-U&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-WoD3pdbPK0zzF7FYOBTCX3y817m3FpPvZ6J4eUEhsQQ&oe=632559B6'),
   modelStatus(
       name: "ahmed ",
       time: '2 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296101279_1532308990516316_550304841361999915_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF8JRAYTCxArkAPtZFEuuVrE-SvCNO-lBYT5K8I076UFrKzyFrQm5xgNNioyM8xUZVkPn68n0t_gKr253PJVhgs&_nc_ohc=bDrqghR8PukAX8F9vJ5&_nc_ht=scontent.fcai19-4.fna&oh=00_AT9sMWNzYPghj5ih84SPDLfdhvM5aVKseoeZVdF9HD_Rkg&oe=63197455'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296101279_1532308990516316_550304841361999915_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF8JRAYTCxArkAPtZFEuuVrE-SvCNO-lBYT5K8I076UFrKzyFrQm5xgNNioyM8xUZVkPn68n0t_gKr253PJVhgs&_nc_ohc=WaEL6wVbFoEAX-ULUAG&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-m3tkknbcvYY52FHWRQL1uyMijTZW_6eZyepljsVgTtg&oe=632551D5'),
   modelStatus(
       name: "Khedr",
       time: '7 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296285903_821179079248785_3145060403143889362_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGW3tUDmYT-6qZRbhvAk4TmmhPiuhay1NeaE-K6FrLU19hOC42raNdIRoeg4Ho-_feeGMZj8IH98uFj3yt5aC5K&_nc_ohc=epGtGKE1i_8AX_f_d64&_nc_ht=scontent.fcai19-4.fna&oh=00_AT9-O7d3HfMAdIgyXwMm7vcz7jvaxZMTD69Qgd-aECbySg&oe=6317C937'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/273571102_704287967604564_5995356503563951538_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFRZFI89AoUyQOunlL1GkoCLiExBLPAXQ0uITEEs8BdDcRxcSYNIFLCpRacQ9BXgO7lAgrRa0q4jeFJ80iyndu5&_nc_ohc=ZVcuvbyHpjYAX8w0juj&_nc_ht=scontent.fcai19-4.fna&oh=00_AT_7TuDz-2OlIEDlowsnRbGmB8PbV2_IFY3olybD6ezyZQ&oe=63254855'),
   modelStatus(
       name: "yousry",
       time: '8 min ago',
       image:
-          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/297986257_1754739734889082_476247543936616366_n.jpg?stp=cp1_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeESyQV4szGpzjWaZWIsw6Fa2Qs83x9Hp_LZCzzfH0en8np2iMfUfPZmK6qYKFul-fzO7lZ_p2U8bo8s3VmzFsSx&_nc_ohc=hGmfXujmxugAX8gDjg1&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-59jfl2G36pSV4RFJ0PECd7LCdBpAoTGQw9-oKX-awcw&oe=6318B2CF'),
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/295904196_1746690802360642_7704700259405042136_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEvGOKU32Y7xOQMIHIIlVk92nCCYRmHePXacIJhGYd49SYf4UQ68T-vVoyyxUHSenoa9ExndL0FL2-DCq_f6J8t&_nc_ohc=1LR0ZwO7DUcAX-qNQ-U&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-WoD3pdbPK0zzF7FYOBTCX3y817m3FpPvZ6J4eUEhsQQ&oe=632559B6'),
+  modelStatus(
+      name: "ahmed ",
+      time: '2 min ago',
+      image:
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/296101279_1532308990516316_550304841361999915_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF8JRAYTCxArkAPtZFEuuVrE-SvCNO-lBYT5K8I076UFrKzyFrQm5xgNNioyM8xUZVkPn68n0t_gKr253PJVhgs&_nc_ohc=WaEL6wVbFoEAX-ULUAG&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-m3tkknbcvYY52FHWRQL1uyMijTZW_6eZyepljsVgTtg&oe=632551D5'),
+  modelStatus(
+      name: "Khedr",
+      time: '7 min ago',
+      image:
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/273571102_704287967604564_5995356503563951538_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFRZFI89AoUyQOunlL1GkoCLiExBLPAXQ0uITEEs8BdDcRxcSYNIFLCpRacQ9BXgO7lAgrRa0q4jeFJ80iyndu5&_nc_ohc=ZVcuvbyHpjYAX8w0juj&_nc_ht=scontent.fcai19-4.fna&oh=00_AT_7TuDz-2OlIEDlowsnRbGmB8PbV2_IFY3olybD6ezyZQ&oe=63254855'),
+  modelStatus(
+      name: "yousry",
+      time: '8 min ago',
+      image:
+          'https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-1/295904196_1746690802360642_7704700259405042136_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEvGOKU32Y7xOQMIHIIlVk92nCCYRmHePXacIJhGYd49SYf4UQ68T-vVoyyxUHSenoa9ExndL0FL2-DCq_f6J8t&_nc_ohc=1LR0ZwO7DUcAX-qNQ-U&_nc_ht=scontent.fcai19-4.fna&oh=00_AT-WoD3pdbPK0zzF7FYOBTCX3y817m3FpPvZ6J4eUEhsQQ&oe=632559B6'),
 ];
